779fc44ec830997e9bdb0774860e19f9dfc67622
ywu <yifengwucms@gmail.com>
2025-02-19 00:21:47 -0600

    CI: Add the `prettier` plugin to `eslint`

--- ci/config.ts
+++ ci/config.ts
@@ -4,6 +4,8 @@ const DEV_DEPS: string[] = [
     "@eslint/js",
     "@types/jest",
     "eslint",
+    "eslint-config-prettier",
+    "eslint-plugin-prettier",
     "eslint-plugin-react",
     "globals",
     "jest",
--- eslint.config.js
+++ eslint.config.js
@@ -2,13 +2,24 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import prettier from "eslint-config-prettier";
+import pluginPrettier from "eslint-plugin-prettier";
 
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
   {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  pluginJs.configs.recommended,  // standard JS rules
+  ...tseslint.configs.recommended,  // TS rules
+  pluginReact.configs.flat.recommended,  // React rules
+  prettier,  // disable conflicting ESLint rules
+  {
+    plugins: { prettier: pluginPrettier },
+    rules: {
+      "prettier/prettier": "error",  // enforce Prettier formatting via ESLint
+      "no-unused-vars": "warn",  // warn about unused variables
+      "no-console": "off",  // allow console logs
+    },
+  },
 ];
