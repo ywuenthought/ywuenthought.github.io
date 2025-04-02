96d675e4794847f76f57efe9cf62124049f2cfc1
ywu <yifengwucms@gmail.com>
2025-04-02 01:28:51 -0500

    DEV: Provide the redux store to the app

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,19 @@
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 
 import App from "./App";
+import { store } from "./redux/store";
+import React from "react";
 
 const container = document.getElementById("root");
 
 if (!container) throw new Error("Missing #root element");
 
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+);
