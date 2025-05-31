403e9619616f9e60605fc6215752e83ed05d1c2f
ywu <yifengwucms@gmail.com>
2025-05-30 22:04:44 -0500

    DEV: Hook the `Projects` UI component to the app

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Route, Routes } from 'react-router-dom';
 import Experience from './components/Experience';
 import Header from './components/Header';
 import Home from './components/Home';
+import Projects from './components/Projects';
 import { ROUTES } from './const';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route path={ROUTES.HOME} element={<Home />} />
           <Route path={ROUTES.EXPERIENCE} element={<Experience />} />
+          <Route path={ROUTES.PROJECTS} element={<Projects />} />
         </Routes>
       </Box>
     </>
