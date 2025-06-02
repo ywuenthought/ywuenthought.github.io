103f92e003bba753b8045a5019425e95b9955ddf
ywu <yifengwucms@gmail.com>
2025-06-01 20:57:27 -0500

    FIX: Add the missing copyright headers

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,7 @@
+// Copyright (c) 2025 Yifeng Wu
+// This file is part of incredible-me and is licensed under the MIT License.
+// See the LICENSE file for more details.
+
 import './index.css';
 
 import { StrictMode } from 'react';
