819cc4dfb3f77987a8e316ae2e57054d7d60df0d
ywu <yifengwucms@gmail.com>
2025-06-03 21:31:58 -0500

    DEV: Allow for searching publications by title

--- src/components/Publications/index.tsx
+++ src/components/Publications/index.tsx
@@ -2,15 +2,38 @@
 // This file is part of incredible-me and is licensed under the MIT License.
 // See the LICENSE file for more details.
 
-import { Stack } from '@mui/material';
+import { Stack, TextField } from '@mui/material';
+import { useState } from 'react';
 
 import { ITEMS } from './const';
 import Item from './item';
 
 export default function Publications() {
+  const [query, setQuery] = useState('');
+
+  const sortedItems = [...ITEMS].sort((item1, item2) => {
+    const match1 = item1.title
+      .toLocaleLowerCase()
+      .includes(query.toLowerCase());
+    const match2 = item2.title
+      .toLocaleLowerCase()
+      .includes(query.toLowerCase());
+
+    if (match1 === match2) return 0;
+    return match1 ? -1 : 1;
+  });
+
   return (
     <Stack spacing={2}>
-      {ITEMS.map((item, index) => (
+      <TextField
+        fullWidth
+        label="Search By Title"
+        value={query}
+        variant="outlined"
+        sx={{ mb: 2 }}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      {sortedItems.map((item, index) => (
         <Item
           {...item}
           key={`publications:items:${index}`}
