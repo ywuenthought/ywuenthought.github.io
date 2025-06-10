c1c64496a1cb527165e60d1448120a4caba6ef3f
ywu <yifengwucms@gmail.com>
2025-06-10 01:49:48 -0500

    DEV: Simplify the travels page for better UI/UX

--- src/components/Travels/index.tsx
+++ src/components/Travels/index.tsx
@@ -2,50 +2,30 @@
 // This file is part of incredible-me and is licensed under the MIT License.
 // See the LICENSE file for more details.
 
-import { Box, Typography } from '@mui/material';
-import { useEffect, useState } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
 
 import { dateTimes, locations, scenes } from './const';
 import Scene from './scene';
-import Slider from './slider';
-
-const UNIT_VALUE = 40;
 
 export default function Travels() {
-  const [index, setIndex] = useState(0);
-  const [vw, setVW] = useState(() => window.innerWidth / 100);
-
-  useEffect(() => {
-    const handleResize = () => setVW(window.innerWidth / 100);
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
   return (
     <Box>
-      <Typography sx={{ fontSize: 'clamp(0.875rem, 2.5vw, 1rem)', mb: 1 }}>
-        {dateTimes[index]}
-      </Typography>
-      <Slider cardWidth={UNIT_VALUE * vw} indexState={{ index, setIndex }}>
-        {scenes.map((scene, index) => (
-          <Scene
-            {...scene}
-            key={`travels:scenes:${index}`}
-            sx={{ width: `${UNIT_VALUE}vw` }}
-          />
-        ))}
-      </Slider>
-      <Typography
-        sx={{
-          fontSize: 'clamp(0.875rem, 2.5vw, 1rem)',
-          mt: 1,
-          whiteSpace: 'normal',
-          width: UNIT_VALUE * vw,
-          wordBreak: 'break-word',
-        }}
-      >
-        {locations[index]}
-      </Typography>
+      {scenes.map((scene, index) => (
+        <Stack key={`travels:items:${index}`} sx={{ mb: 4 }}>
+          <Typography
+            sx={{
+              fontSize: 'clamp(0.875rem, 2.5vw, 1rem)',
+              mb: 1,
+              whiteSpace: 'normal',
+              width: '50vw',
+              wordBreak: 'break-word',
+            }}
+          >
+            {dateTimes[index]} - {locations[index]}
+          </Typography>
+          <Scene {...scene} sx={{ width: '50vw' }} />
+        </Stack>
+      ))}
     </Box>
   );
 }
