2095b8f7dfafd9fcb9593ef46c1ffb86f001e299
ywu <yifengwucms@gmail.com>
2025-06-10 00:16:05 -0500

    DEV: Present the header as a hamburger menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,8 +2,19 @@
 // This file is part of incredible-me and is licensed under the MIT License.
 // See the LICENSE file for more details.
 
+import { Menu } from '@mui/icons-material';
 import type { SxProps, Theme } from '@mui/material';
-import { AppBar, Box, Button, Toolbar } from '@mui/material';
+import {
+  AppBar,
+  Box,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  Toolbar,
+} from '@mui/material';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { ROUTES } from '@/const';
@@ -18,23 +29,41 @@ type HeaderProps = {
 export default function Header(props: HeaderProps) {
   const { theme, sx = {} } = props;
 
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
   return (
-    <AppBar color="default" elevation={0} position="fixed" sx={{ ...sx }}>
-      <Toolbar>
-        {Object.entries(ROUTES).map(([name, path]) => (
-          <Button
-            color="inherit"
-            component={Link}
-            key={`header:${name.toLowerCase()}`}
-            sx={{ textTransform: 'none', mx: 1 }}
-            to={path}
-          >
-            {name}
-          </Button>
-        ))}
-        <Box sx={{ flexGrow: 1 }} />
-        <ThemeController {...theme} />
-      </Toolbar>
-    </AppBar>
+    <>
+      <AppBar color="default" elevation={0} position="fixed" sx={{ ...sx }}>
+        <Toolbar>
+          <IconButton edge="end" onClick={() => setDrawerOpen(true)}>
+            <Menu />
+          </IconButton>
+          <Box sx={{ flexGrow: 1 }} />
+          <ThemeController {...theme} />
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        anchor="left"
+        open={drawerOpen}
+        onClose={() => setDrawerOpen(false)}
+      >
+        <Box sx={{ width: 200 }} onClick={() => setDrawerOpen(false)}>
+          <List>
+            {Object.entries(ROUTES).map(([name, path]) => (
+              <ListItem key={`header:${name.toLowerCase()}`}>
+                <ListItemButton
+                  color="inherit"
+                  component={Link}
+                  sx={{ textTransform: 'none', mx: 1 }}
+                  to={path}
+                >
+                  {name}
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Drawer>
+    </>
   );
 }
