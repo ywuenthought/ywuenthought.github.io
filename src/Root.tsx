9ab1adf07812e53b5345c3e4a23a6ce4caf2363c
ywu <yifengwucms@gmail.com>
2025-06-11 20:24:30 -0500

    FIX: Use `HashRouter` to avoid 404 when refreshing on tabs that are not home

--- src/Root.tsx
+++ src/Root.tsx
@@ -4,7 +4,7 @@
 
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { StrictMode, useState } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import App from './App.tsx';
 import { darkTheme, lightTheme } from './components/Header/const.ts';
@@ -16,9 +16,9 @@ export default function Root() {
     <StrictMode>
       <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
         <CssBaseline />
-        <BrowserRouter>
+        <HashRouter>
           <App theme={{ isDark, setMode }} />
-        </BrowserRouter>
+        </HashRouter>
       </ThemeProvider>
     </StrictMode>
   );
